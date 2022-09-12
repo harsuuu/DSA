c355fa6daaa43ffa86c519d0e45ec484b4c4ec92
harsuuu <harshjoshi7895@gmail.com>
2022-09-12 11:21:51 +0530

    Head recursion

---
+++ Chapter 2 --- Recursion/recursion9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void fun(int n)
+{
+    int i=1;
+    while(i<=n)
+    {
+        
+        printf("%d\n",i);
+        i++;
+       
+    }
+}
+int main(){
+int x=5;
+fun(x);
+
+return 0;
+}
